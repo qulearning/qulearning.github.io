f79adaea64928865a7ebb47771f08d9468f80112
Elvin Hu <elvinhu1016@gmail.com>
2025-12-05 17:47:05 -0800

    Remove gatekeeping logic to ensure file fetching works

--- files/final_touches.js
+++ files/final_touches.js
@@ -1,18 +1,4 @@
 (function () {
-    let allow = null;
-    try {
-        allow = sessionStorage.getItem('allow_game');
-    } catch (e) {}
-
-    if (!allow) {
-        location.replace('/');
-        return;
-    }
-
-    try {
-        sessionStorage.removeItem('allow_game');
-    } catch (e) {}
-
     const cleanURL = "https://qulearning.github.io";
 
     if (location.href !== cleanURL) {
